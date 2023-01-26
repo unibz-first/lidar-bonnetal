e03027b0d931ff2b4d7b22df2af983d10a6d0f22
XuRobotics <liuxu@seas.upenn.edu>
2023-01-25 22:44:47 -0500

    update yaml files and defaults to train on virginia tech dataset

--- full_data_preprocessor.py
+++ full_data_preprocessor.py
@@ -3,7 +3,7 @@ import time
 from train_test_split import train_test_split_save
 from convert_images_to_labels_pcds import convert_images_to_labels_pcds
 
-for_jackle = True
+for_jackle = False
 for_indoor = False
 convert_images_to_labels_pcds(for_jackle, for_indoor)
 print('convert_images_to_labels_pcds finished, starting training and test set split in 2 seconds...')
