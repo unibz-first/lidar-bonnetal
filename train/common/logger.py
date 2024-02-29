7196059ed45c691f2547984f54515de3471b124a
mycochang <mchang@unibz.it>
2024-02-29 23:30:14 +0100

    tf 2.x update from tf 1.x og

--- train/common/logger.py
+++ train/common/logger.py
@@ -13,14 +13,18 @@ class Logger(object):
 
   def __init__(self, log_dir):
     """Create a summary writer logging to log_dir."""
-    self.writer = tf.summary.FileWriter(log_dir)
+    self.writer = tf.summary.create_file_writer(log_dir)
 
   def scalar_summary(self, tag, value, step):
     """Log a scalar variable."""
-    summary = tf.Summary(
-        value=[tf.Summary.Value(tag=tag, simple_value=value)])
-    self.writer.add_summary(summary, step)
-    self.writer.flush()
+    with self.writer.as_default():
+    # old tensorflow=v1.x
+    # summary = tf.summary(
+    #     value=[tf.summary.Value(tag=tag, simple_value=value)])
+    # self.writer.add_summary(summary, step)
+    #new tf=v2.x  
+      tf.summary.scalar(tag, value, step=step)
+      self.writer.flush()
 
   def image_summary(self, tag, images, step):
     """Log a list of images."""
